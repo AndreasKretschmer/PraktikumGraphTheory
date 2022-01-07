088008f32d1b26d58eeb77540682a8574ed12f9b
Andreas Kretschmer <andy.kretschmer@web.de>
2022-01-07 16:06:46 +0100

    added function to use archived files

--- gen_class_pipe.py
+++ gen_class_pipe.py
@@ -1,27 +1,46 @@
 from numpy.core.fromnumeric import size
 from erdbeermet.simulation import simulate
+from erdbeermet.simulation import load
 from erdbeermet.recognition import recognize
 from erdbeermet.visualize.BoxGraphVis import plot_box_graph
 import numpy as np
 import os
 import timeit
 
+def write_simulation_to_file(circular, clocklike, size, history, id):
+    filename = f'{id}_simulation_circular{circular}_clocklike_{clocklike}_size{size}'
+    filename = os.path.join(os.getcwd(), 'sim',filename)
+    history.write_history(filename)
+
+def load_simulations_from_files():
+    histories = []
+    for file in next(os.walk(os.path.join(os.getcwd(), 'sim')), (None, None, []))[2]:
+        histories.append(load(os.path.join(os.getcwd(),'sim', file)))
+    
+    return histories
 
 def create_diff_simulations():
     histories = []
     runtimes = []
-    for bool in (True, False):
-        for bool2 in (True, False):
-            start = timeit.default_timer()
+    for i in range(15):
+        for bool in (True, False):
+            for bool2 in (True, False):
+                start = timeit.default_timer()
+                size = np.random.randint(6, 10)
+                history = simulate(size, branching_prob=0.0, circular=bool, clocklike=bool2)
+                histories.append(history)
 
-            histories.append(simulate(np.random.randint(
-                4, 9), branching_prob=0.0, circular=bool, clocklike=bool2))
-
-            stop = timeit.default_timer()
-            runtimes.append(stop - start)
+                stop = timeit.default_timer()
+                runtimes.append(stop - start)
+                write_simulation_to_file(bool, bool2, size, history, i)
 
     return histories, runtimes
 
+def get_simulations(import_from_file = False):
+    if import_from_file:
+        return load_simulations_from_files(), []
+    else:
+        return create_diff_simulations()
 
 def compare_first_leaves(history, rec_tree):
     first_leaves = history.history[:4]
@@ -37,7 +56,7 @@ def compare_first_leaves(history, rec_tree):
             # print(
             #     f'Candidate: {node.V}.......... Actual first 4 leaves: {leaves_indices}')
 
-            if (node.V == leaves_indices):
+            if (set(node.V) == set(leaves_indices)):
                 leaves_equal = True
 
     return leaves_equal
@@ -78,22 +97,22 @@ def recognize_histories(histories, first_candidate_only=True, print_info=False):
         is_rmap = (rec_tree.root.valid_ways > 0)
 
         if is_rmap:
-            print('Is R-Map!')
-
-            leaves_equal = compare_first_leaves(history, rec_tree)
-            print(
-                f'Are first 4 simulation leaves and final 4-leaf map equal? {leaves_equal}')
-
-            common_triple_cnt = divergence_measure(history, rec_tree)
-            print(f'Common triples: {common_triple_cnt}')
-
-            print('\n')
-
+            handle_reconstruction_success(history, rec_tree)
         else:
             handle_reconstruction_failure()
 
     return runtimes
 
+def handle_reconstruction_success(history, rec_tree):
+    print('Is R-Map!')
+
+    leaves_equal = compare_first_leaves(history, rec_tree)
+    print(f'Are first 4 simulation leaves and final 4-leaf map equal? {leaves_equal}')
+
+    common_triple_cnt = divergence_measure(history, rec_tree)
+    print(f'Common triples: {common_triple_cnt}')
+
+    print('\n')
 
 def handle_reconstruction_failure(rec_tree):
     print('reconstruction failed')
@@ -111,16 +130,18 @@ def handle_reconstruction_failure(rec_tree):
 
 
 def average_runtimes(sim_runtimes, rec_runtimes):
+    print(f'# sim runtimes: {len(sim_runtimes)}')
     print(f'Average simulation time: {np.mean(sim_runtimes)}s')
 
+    print(f'# rec runtimes: {len(rec_runtimes)}')
     print(f'Average recognition runtime: {np.mean(rec_runtimes)}s')
 
 
 def __main__():
-    histories, sim_runtimes = create_diff_simulations()
+    # histories, sim_runtimes = create_diff_simulations()
+    histories, sim_runtimes = get_simulations(import_from_file=True)
     rec_runtimes = recognize_histories(histories)
 
     average_runtimes(sim_runtimes, rec_runtimes)
-
 
 __main__()
